f2a0119912c62543654a0cd72aecd60e2dfcde33
Weugene <weugene@mail.ru>
2023-08-14 21:12:55 +0400

    translation

--- wp-content/themes/wplms-child/Tit ary – SUOT_files/input_limiter.js
+++
@@ -1,6 +0,0 @@
-jQuery( document ).on( 'input', 'input[data-setting="widget_id"]', function( $ ) {
-    if (this.value.length > 20)
-        console.log("Too long input widget id. It must be less than 20");
-    this.maxLength = 20;
-});
- 
